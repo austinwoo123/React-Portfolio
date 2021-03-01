def9fc48d78fb1b23b0169030eb25fedeabf8d5c
austinwoo123 <austinwoo123@gmail.com>
2021-03-01 13:01:31 -0800

    create header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './App.css';
 import Navbar from "./components/Navbar";
-
-
+import Header from "./components/Header";
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Header />
+    </>
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./style.css"
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+            <div className="main-info">
+                <h1>Welcome to my Portfolio!</h1>
+            </div>
+        </div>
+    )
+}
+export default Header;
