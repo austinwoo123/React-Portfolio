6a4d6aeb0f57fa018bc3e0bdd8cdc3f225041429
austinwoo123 <austinwoo123@gmail.com>
2021-04-07 19:32:25 -0700

    clean up code in footer file

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -14,26 +14,26 @@ import style from "./style.css"
 
 export default function Footer() {
     return (
-        <div>
-            <div className="container">
-                <div className="card bg-white text-white">
-                    <div className="row">
-                        {/* <div className="col"> */}
-                        {/* <div className="col-6"> */}
-                        <h5 className="card-title">Austin Woo</h5>
-                        <div class='footer-link-items'>
-                            <a href='https://austin-woo.herokuapp.com/'>Portfolio</a> <br />
-                            <a href='https://github.com/austinwoo123'>Github</a> <br />
-                            <a href='https://www.linkedin.com/in/awoo95/'>LinkedIn</a> <br />
-                            <a href='mailto: awoo004@ucr.edu'>Email</a>
-                        </div>
-                        {/* </div> */}
-                        {/* </div> */}
-
-                        <small className='website-rights'> &copy;{new Date().getFullYear()} MIT | All rights reserved</small>
-                    </div>
-                </div>
+        // <div>
+        <div className="container">
+            {/* <div className="card bg-white text-white"> */}
+            {/* <div className="row"> */}
+            {/* <div className="col"> */}
+            {/* <div className="col-6"> */}
+            <h5 className="card-title">Austin Woo</h5>
+            <div class='footer-link-items'>
+                <a href='https://austin-woo.herokuapp.com/'>Portfolio</a> <br />
+                <a href='https://github.com/austinwoo123'>Github</a> <br />
+                <a href='https://www.linkedin.com/in/awoo95/'>LinkedIn</a> <br />
+                <a href='mailto: awoo004@ucr.edu'>Email</a>
             </div>
+            {/* </div> */}
+            {/* </div> */}
+
+            <small className='website-rights'> &copy;{new Date().getFullYear()} MIT | All rights reserved</small>
+            {/* </div> */}
+            {/* </div> */}
         </div>
+        // </div>
     )
 }
